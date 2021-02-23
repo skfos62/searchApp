6cf45b62a270271758597ffba710451d2ab0aa5c
skfos62 <almighty62@naver.com>
2021-02-24 02:42:59 +0900

    Initial Commit

---
+++ searchApp/searchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  searchAppApp.swift
+//  searchApp
+//
+//  Created by 정나래 on 2021/02/24.
+//
+
+import SwiftUI
+
+@main
+struct searchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
